5284b3d44daf787ce0d1596cd10f39964712efa4
Moneexa <14besemsyed@seecs.edu.pk>
2020-04-01 18:56:43 +0500

    Add files via upload

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Layout from './shared/components/layout/layout';
+import { Provider } from "react-redux";
+import store from "./shared/store/store";
 
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Layout />
+    </Provider>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
