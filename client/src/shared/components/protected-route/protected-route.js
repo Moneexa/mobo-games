2febe8bcf2aa492157554174cba32846e2382b65
Moneexa <14besemsyed@seecs.edu.pk>
2020-04-01 19:01:19 +0500

    Add files via upload

--- client/src/shared/components/protected-route/protected-route.js
+++ client/src/shared/components/protected-route/protected-route.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import { Route, Redirect } from "react-router-dom";
 
+export const ProtectedRoute = ({ component: Component, redirect, canActivate, ...rest }) => (
+    <Route
+        {...rest}
+        render={props =>
+            canActivate ? (
+                <Component {...props} />
+            ) : (
+                    <Redirect
+                        to={{
+                            pathname: redirect,
+                            state: { from: props.location }
+                        }}
+                    />
+                )
+        }
+    />
+)
